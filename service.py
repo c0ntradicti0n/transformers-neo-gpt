096a0e2b1a010abae6011feb669c3fd7676aa830
stefan-werner <stefan.werner@infai.org>
2022-02-03 11:00:36 +0100

    Initial commit

--- service.py
+++ service.py
@@ -1,5 +1,7 @@
 from transformers import pipeline
+
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
 
-def f(intent=None, **other_kwargs:)
-    print (generator("EleutherAI has", do_sample=True, min_length=50))
+
+def f(intent=None, **other_kwargs):
+    print(generator(intent, do_sample=True, min_length=50))
