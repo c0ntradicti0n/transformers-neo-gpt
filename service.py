faaeb6d57df53e6f9f34c08041f4ff1f5c89c8d1
stefan-werner <stefan.werner@infai.org>
2022-02-03 12:44:37 +0100

    Add return

--- service.py
+++ service.py
@@ -5,4 +5,8 @@ generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
 
 def f(intent=None, **other_kwargs):
     text = "".join(intent.split()[:10])
-    print(generator(text, do_sample=True, min_length=50))
+    output = (generator(text, do_sample=True, min_length=50))
+    print (f"Made this new idea {output}")
+    return {
+        "idea": output
+    }
