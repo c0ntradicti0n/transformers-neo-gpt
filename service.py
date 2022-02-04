5819c7fea40a38778574b09c5b61bcbaa327089c
stefan-werner <stefan.werner@infai.org>
2022-02-04 15:54:56 +0100

    userinput

--- service.py
+++ service.py
@@ -5,7 +5,8 @@ model_name = os.environ["model_name"] if "model_name" in os.environ else 'Eleuth
 generator = pipeline('text-generation', model=model_name)
 
 
-def f(intent=None, **other_kwargs):
+def f(intent=None, userinput=None, **other_kwargs):
+    string = query=userinput if userinput else intent
     text = "".join(intent.split()[:10])
     output = (generator(text, do_sample=True, min_length=50))
     print (f"Made this new idea {output}")
