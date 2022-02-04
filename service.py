e5e67db2f00900d3011faba4fef09ce46cdfcf53
stefan-werner <stefan.werner@infai.org>
2022-02-04 16:36:29 +0100

    allow do adjust length

--- service.py
+++ service.py
@@ -3,13 +3,13 @@ import os
 
 model_name = os.environ["model_name"] if "model_name" in os.environ else 'EleutherAI/gpt-neo-125M'
 generator = pipeline('text-generation', model=model_name)
-
+length = os.environ["length"] if "length" in os.environ else 150
 
 def f(intent=None, userinput=None, **other_kwargs):
     string = userinput if userinput else intent
     text = "".join(string.split()[:10]).replace('"', '').replace("[", '').replace("]", '')
     print ("text" + string + text)
-    output = (generator(text, do_sample=True, min_length=50))
+    output = (generator(text, do_sample=True, min_length=length))
     print (f"Made this new idea {output}")
     return {
         "idea": output
