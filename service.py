6642dfcc3c33ee66d943875037575becc3dd56f9
stefan-werner <stefan.werner@infai.org>
2022-02-07 17:03:10 +0100

    add spaces again

--- service.py
+++ service.py
@@ -7,7 +7,7 @@ length = os.environ["length"] if "length" in os.environ else 150
 
 def f(intent=None, userinput=None, **other_kwargs):
     string = userinput if userinput else intent
-    text = "".join(string.split()[:10]).replace('"', '').replace("[", '').replace("]", '')
+    text = " ".join(string.split()[:10]).replace('"', '').replace("[", '').replace("]", '')
     print ("text" + string + text)
     output = (generator(text, do_sample=True, min_length=length))
     print (f"Made this new idea {output}")
